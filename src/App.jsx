b7746ca8a6eff2cc950b70ac28f17468759364ea
sebasdurango <sebasdurango19@gmail.com>
2024-07-04 20:19:07 -0500

    Create Components

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+import { Covenas1 } from './components/Covenas1'
+import { Covenas2 } from './components/Covenas2'
+import { Covenas3 } from './components/Covenas3'
+import { Covenas4 } from './components/Covenas4'
+import { Covenas5 } from './components/Covenas5'
+import { Covenas6 } from './components/Covenas6'
+
+function App() {
+  
+  return (
+    <>
+      <Covenas1/>
+      <Covenas2/>
+      <Covenas3/>
+      <Covenas4/>
+      <Covenas5/>
+      <Covenas6/>
+    </>
+  )
+}
+
+export default App
